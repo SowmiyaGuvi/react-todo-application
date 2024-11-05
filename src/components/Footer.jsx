441fcf07fb893c3b13142f1155b5b12483501b84
SowmiyaGuvi <sowmimiya43@gmail.com>
2024-11-05 13:16:33 +0530

    todo application added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+      <>
+      <footer className="py-5 mt-3 bg-dark">
+          <div className="container">
+              <p className="m-0 text-center text-white">
+                  Copyright <a href="mailTo:rajkumaranbu192@gmail.com">©rajkumaranbu192@gmail.com</a> 2024
+              </p>
+          </div>
+      </footer>
+      </>
+    )
+  }
+  
+  export default Footer
